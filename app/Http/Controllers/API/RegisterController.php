2c9e425e5efce741db674d8c441da02f46e5733e
Nicholas Michael <nicholasm185@gmail.com>
2020-05-20 14:55:45 +0000

    add name to login function

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -15,6 +15,7 @@ class RegisterController extends BaseController
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
             $success['token'] =  $user->createToken('MyApp')-> accessToken;
+            $success['name'] = $user['name'];
             return response()->json(['success' => $success], 200);
         }
         else{
